88ff92090f7ec852a7ae7d6da2a41b8b91f37a36
siddheshThorat04 <siddheshthorat2004@gmail.com>
2025-03-06 21:50:20 +0530

    unAuthorized Issue

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json()); // allows us to parse incoming requests:req.body
 app.use(cookieParser()); // allows us to parse incoming cookies
 
-app.use(cors({ origin: ["http://localhost:5173", process.env.CLIENT_URL], credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 
 app.use("/api/auth", authRoutes)
