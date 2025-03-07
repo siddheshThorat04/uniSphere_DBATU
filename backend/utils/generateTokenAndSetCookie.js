a8be694d6e58d0dd1f78d58f9b39408b59e81723
siddheshThorat04 <siddheshthorat2004@gmail.com>
2025-03-07 11:53:27 +0530

    cookies Samesite Problem

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -9,7 +9,7 @@ export const generateTokenAndSetCookie = ( userId,res) => {
 	res.cookie("token", token, {
 		httpOnly: true,
 		secure: process.env.NODE_ENV === "production",
-		sameSite: "strict",
+		sameSite: "none",
 		maxAge: 30*24*60*60*1000
 	});
 
